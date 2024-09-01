de23456cd52debf763061fa2e5e486be593f4049
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2024-09-01 19:11:20 +0530

    chore: Update socketIoClient.js to use WEBSOCKET_URL instead of VITE_BACKEND_URL

--- src/api/socketIoClient.js
+++ src/api/socketIoClient.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 // Initialize socket connection
-const socket = io(import.meta.env.VITE_BACKEND_URL, {
+const socket = io(import.meta.env.WEBSOCKET_URL, {
   withCredentials: true,
 });
 
